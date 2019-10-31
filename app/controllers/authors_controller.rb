9cd5f2d4de5c75ff0408dd252462b6b66881684c
swietheger <swietheger@universum.com>
2019-10-31 15:00:06 +0100

    Passing test for #6

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,11 +1,18 @@
 class AuthorsController < ApplicationController
+
+    def show
+        @author = Author.find(params[:id])
+      end
+
     def new
         @author = Author.new
     end
 
     def create
         @author = Author.new(author_params)
+        
         @author.save
+        redirect_to @author
     end
 
     private
--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 describe "Show author page", type: :feature do
   
-  FactoryBot.create :author
-
   it "should render withour error" do
+    @alan = FactoryBot.create :author
     visit author_path(@alan)
   end
-
   it "should have a text describing the authors properties" do
+    @alan = FactoryBot.create :author
     visit author_path(@alan)
-    expect(page).to have_text('This is Alan Turing. Checkout his homepage http://wikipedia.de/Alan_Turing')
+    expect(page).to have_text("Alan Turing. Checkout his homepage http://wikipedia.de/Alan_Turing.")
   end
 end
