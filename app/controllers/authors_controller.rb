3ba1f6758980354d35df930e9586bc61f1e21dfa
swietheger <swietheger@universum.com>
2019-10-31 18:21:44 +0100

    Pass for test #12

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,6 +12,10 @@ class AuthorsController < ApplicationController
         @author = Author.new
     end
 
+    def edit
+        @author = Author.find(params[:id])
+    end
+
     def create
         @author = Author.new(author_params)
 
@@ -21,6 +25,16 @@ class AuthorsController < ApplicationController
             render 'new'
         end
     end
+
+    def update
+        @author = Author.find(params[:id])
+       
+        if @author.update(author_params)
+          redirect_to @author
+        else
+          render 'edit'
+        end
+      end
 
     private
         def author_params
