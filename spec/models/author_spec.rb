10ebe8f2126164f1dc043a48125d286254aa8e6f
swietheger <swietheger@universum.com>
2019-10-31 13:22:47 +0100

    Pass #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,11 +5,11 @@ describe "Author model", type: :model do
   author_last_name = 'Turing'
   author_homepage = 'http://wikipedia.org/Alan_Turing'
   
-  it "should have strings first name, lastname and homepage" do
-    author = Author.new(author_first_name, author_last_name, author_homepage)
+  it "should have strings first_name, last_name and homepage" do
+    author = Author.new({:first_name => author_first_name, :last_name => author_last_name, :homepage =>author_homepage})
 
     expect(author.first_name).to eq(author_first_name)
-    expect(author.first_name).to eq(author_last_name)
-    expect(author.first_name).to eq(author_homepage)
+    expect(author.last_name).to eq(author_last_name)
+    expect(author.homepage).to eq(author_homepage)
   end
 end
