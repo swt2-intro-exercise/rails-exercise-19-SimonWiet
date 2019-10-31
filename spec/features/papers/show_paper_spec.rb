a75370a1a08879452d43f70504dc2a427779b50d
swietheger <swietheger@universum.com>
2019-10-31 20:32:41 +0100

    #18

---
+++ spec/features/papers/show_paper_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Show author page", type: :feature do
+  
+  it "should render withour error" do
+    @alan = FactoryBot.create :paper
+    visit author_path(@alan)
+  end
+  it "should have a text describing the authors properties" do
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+    expect(page).to have_text("Alan Turing. Checkout his homepage http://wikipedia.de/Alan_Turing.")
+  end
+end
